fe259016028e1f22299143196296bfc958653e16
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 19:25:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size < 3
     array.join(" and ")
   else
-    string = array.join(", ")
-    string.insert(-2, "and")
+    array.insert(-2, "and")
+    array.join(", ")
   end
 end
