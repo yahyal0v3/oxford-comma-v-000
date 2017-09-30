d11675c9eac601ab01430f2341b446c755f33f18
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 19:34:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    array.join(" and ")
+  else
+    array.insert(-2, "and")
+    string = array.join(", ")
+    string["and,"] = "and"
+    string
+  end
 end
